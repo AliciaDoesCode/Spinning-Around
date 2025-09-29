079208011acd5669c8bad169d79aa730a677b9e8
Alicia <95318927+AliciaDoesCode@users.noreply.github.com>
2025-09-29 10:21:06 +0100

    Update welcome.js

--- src/events/guildMemberAdd/welcome.js
+++ src/events/guildMemberAdd/welcome.js
@@ -2,7 +2,6 @@ console.log('guildMemberAdd event fired');
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = async (member) => {
-  // Replace with your welcome channel ID or comment out if you don't have a welcome channel
   const channel = member.guild.channels.cache.get('1421954415786459211');
   if (!channel) return;
 
